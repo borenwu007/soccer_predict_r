077fab4c3405d816f05de713379349d5c84ec195
borenwu007 <borenwu10@163.com>
2020-01-16 22:08:21 +0800

    4444

---
+++ client/src/renderer/components/Predictor/Predictor.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p>{{msg}} world!</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                msg: 'hi'
+            }
+        },
+
+        methods: {
+            hello() {
+                console.log("hello world!")
+            }
+        },
+    }
+</script>
+
+<style>
+
+</style>
